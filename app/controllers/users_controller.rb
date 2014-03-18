e21edd723baecd0b41b12439afcbd674ec22aeda
Yasuhiro Usutani <y_usutani@me.com>
2014-03-18 15:07:41 +0900

    jbuilderを使わない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,11 @@ class UsersController < ApplicationController
   end
 
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
   end
 
@@ -30,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     h = user_params.select { |key,_| vpp_user_key_filter.include? key }
     h["client_user_id_str"] = SecureRandom.uuid
@@ -39,44 +36,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.client_user_id_str = h["client_user_id_str"]
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     h = user_params.select { |key,_| vpp_user_key_filter.include? key }
     Vpp::Application.config.vpp_client.edit_user(h.symbolize_keys)
 
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     Vpp::Application.config.vpp_client.retire_user(client_user_id_str: @user.client_user_id_str)
 
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    redirect_to users_url
   end
 
   private
--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :client_user_id_str, :email, :its_id_hash, :status, :user_id, :created_at, :updated_at
