410457433e4ddf50cf8c9a2ef045499c7eb2c308
Yasuhiro Usutani <y_usutani@me.com>
2014-03-26 16:25:04 +0900

    LicensesControllerTestにモックを導入

--- test/controllers/licenses_controller_test.rb
+++ test/controllers/licenses_controller_test.rb
@@ -2,7 +2,24 @@ require 'test_helper'
 
 class LicensesControllerTest < ActionController::TestCase
   setup do
+    @user = users(:one)
     @license = licenses(:one)
+  end
+
+  test "should sync licenses" do
+    h = { licenses: Array.new }
+    Vpp::Application.config.vpp_client.stubs(:get_licenses).returns(h)
+    get :sync
+  end
+
+  test "should associate license with user" do
+    Vpp::Application.config.vpp_client.stubs(:associate_license_with_user)
+    get :associate, id: @license, user_id: @user
+  end
+
+  test "should disassociate license from user" do
+    Vpp::Application.config.vpp_client.stubs(:disassociate_license_from_user)
+    get :disassociate, id: @license
   end
 
   test "should get index" do
@@ -11,49 +28,8 @@ class LicensesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:licenses)
   end
 
-  test "should get new" do
-=begin
-    get :new
-    assert_response :success
-=end
-  end
-
-  test "should create license" do
-=begin
-    assert_difference('License.count') do
-      post :create, license: { adam_id: @license.adam_id, client_user_id_str: @license.client_user_id_str, is_irrevocable: @license.is_irrevocable, its_id_hash: @license.its_id_hash, license_id: @license.license_id, pricing_param: @license.pricing_param, product_type_id: @license.product_type_id, product_type_name: @license.product_type_name, status: @license.status, user_id: @license.user_id }
-    end
-
-    assert_redirected_to license_path(assigns(:license))
-=end
-  end
-
   test "should show license" do
     get :show, id: @license
     assert_response :success
-  end
-
-  test "should get edit" do
-=begin
-    get :edit, id: @license
-    assert_response :success
-=end
-  end
-
-  test "should update license" do
-=begin
-    patch :update, id: @license, license: { adam_id: @license.adam_id, client_user_id_str: @license.client_user_id_str, is_irrevocable: @license.is_irrevocable, its_id_hash: @license.its_id_hash, license_id: @license.license_id, pricing_param: @license.pricing_param, product_type_id: @license.product_type_id, product_type_name: @license.product_type_name, status: @license.status, user_id: @license.user_id }
-    assert_redirected_to license_path(assigns(:license))
-=end
-  end
-
-  test "should destroy license" do
-=begin
-    assert_difference('License.count', -1) do
-      delete :destroy, id: @license
-    end
-
-    assert_redirected_to licenses_path
-=end
   end
 end
