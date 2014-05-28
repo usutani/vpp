930143ffca83262299eed252ab94c4a279edfa51
Yasuhiro Usutani <usutani@tgl.co.jp>
2014-05-28 14:21:58 +0900

    Rails 4.1.1に対応

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Vpp::Application.initialize!
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Vpp::Application.config.session_store :cookie_store, key: '_vpp_session'
+Rails.application.config.session_store :cookie_store, key: '_vpp_session'
